a0de59065b2975490a147ff1e4a5f0f020b4f03f
ernestofurchtgott <ernestofg2587@gmail.com>
2021-05-21 11:56:55 -0400

    Add files via upload

---
+++ Proj1_b.js
@@ -0,0 +1,23 @@
+ d3
+.select("#data_2")
+.append('ul')
+.attr("id","data_list");
+
+const list = d3
+.select("ul");
+
+
+list
+.append('li')
+.text('Resources / events / tools');
+
+
+list.append("li").text('Design references - Project 1');
+list.append("li").text('prueba');
+list.append("li").text('prueba');
+list.append("li").text('prueba');
+list.append("li").text('prueba');
+list.append("li").text('prueba');
+
+
+ 
